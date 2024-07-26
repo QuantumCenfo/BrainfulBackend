5134140499e4a0bebf2b2e9b675436c9b9c6baa6
AlejandroPenarandaSoto <135558117+AlejandroPenarandaSoto@users.noreply.github.com>
2024-07-25 18:54:32 -0600

    Backend ChallengeOutdoor

--- src/main/java/com/project/demo/rest/challengeoutdoor/ChallengeOutdoorRestController.java
+++ src/main/java/com/project/demo/rest/challengeoutdoor/ChallengeOutdoorRestController.java
@@ -7,9 +7,7 @@ import com.project.demo.logic.entity.challengeroutdoor.ChallengeOutdoor;
 import com.project.demo.logic.entity.challengeroutdoor.ChallengeOutdoorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.List;
@@ -20,10 +18,38 @@ public class ChallengeOutdoorRestController {
 
     @Autowired
     private ChallengeOutdoorRepository challengeOutdoorRepository;
-    @GetMapping
+
+    @GetMapping("/active-challenges")
     @PreAuthorize("hasAnyRole('ADMIN', 'SUPER_ADMIN','USER')")
     public List<ChallengeOutdoor> getAllActiveChallenges() {
         Date currentDate = new Date();
         return challengeOutdoorRepository.findAllActiveChallenges(currentDate);
+    }
+
+    @GetMapping("/inactive-challenges")
+    @PreAuthorize("hasAnyRole('ADMIN', 'SUPER_ADMIN','USER')")
+    public List<ChallengeOutdoor> getAllInactiveChallenges() {
+        Date currentDate = new Date();
+        return challengeOutdoorRepository.findAllInactiveChallenges(currentDate);
+    }
+
+    @PostMapping
+    @PreAuthorize("hasAnyRole('ADMIN', 'SUPER_ADMIN')")
+    public ChallengeOutdoor addChallengeOutdoor(@RequestBody ChallengeOutdoor challengeOutdoor) {
+        return challengeOutdoorRepository.save(challengeOutdoor);
+    }
+
+    @PutMapping("/{id}")
+    public ChallengeOutdoor updateDateChallengeOutdoor(@PathVariable Long id, @RequestBody ChallengeOutdoor challengeOutdoor) {
+        return challengeOutdoorRepository.findById(id)
+                .map(existingChallengeOutdoor -> {
+                    existingChallengeOutdoor.setStartDate(challengeOutdoor.getStartDate());
+                    existingChallengeOutdoor.setEndDate(challengeOutdoor.getEndDate());
+                    return challengeOutdoorRepository.save(existingChallengeOutdoor);
+                })
+                .orElseGet(() -> {
+                    challengeOutdoor.setOutdoorChallengeId(id);
+                    return challengeOutdoorRepository.save(challengeOutdoor);
+                });
     }
 }
