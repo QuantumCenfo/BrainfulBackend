10add6da27061971ebd8325fb58f64a8fa1d044a
IsaacCastroCrfusion <143829154+IsaacCastroCrfusion@users.noreply.github.com>
2025-11-15 19:16:45 -0600

    cambios de tests
    Se agrega al seedes 3 usuarios para los roles, orden mayormente
    Unos cambios del sonasqube
    Se agregar dependencias para usar rest asured y 2 archivos con los test, login, y el de foros que contiene pruebas de foros y de comentario

--- src/main/java/com/project/demo/logic/entity/user/UserSeeder.java
+++ src/main/java/com/project/demo/logic/entity/user/UserSeeder.java
@@ -31,16 +31,68 @@ public class UserSeeder implements ApplicationListener<ContextRefreshedEvent> {
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
+        this.createSuperAdmin();
+        this.createAdmin();
         this.createUser();
+    }
+
+    private void createSuperAdmin() {
+
+        User admin = new User();
+        admin.setName("SuperAdmin01");
+        admin.setLastname("System");
+        admin.setEmail("SuperAdmin01@gmail.com");
+        admin.setPassword("SuperAdmin");
+
+        Optional<Role> optionalAdminRole = roleRepository.findByName(RoleEnum.SUPER_ADMIN);
+        Optional<User> optionalAdminUser = userRepository.findByEmail(admin.getEmail());
+
+        if (optionalAdminRole.isEmpty() || optionalAdminUser.isPresent())  {
+            return;
+        }
+
+        var newASuperAdmin = new User();
+        newASuperAdmin.setName(admin.getName());
+        newASuperAdmin.setLastname(admin.getLastname());
+        newASuperAdmin.setEmail(admin.getEmail());
+        newASuperAdmin.setPassword(passwordEncoder.encode(admin.getPassword()));
+        newASuperAdmin.setRole(optionalAdminRole.get());
+
+        userRepository.save(newASuperAdmin);
+    }
+
+    private void createAdmin() {
+
+        User admin = new User();
+        admin.setName("Admin01");
+        admin.setLastname("System");
+        admin.setEmail("Admin01@gmail.com");
+        admin.setPassword("Admin");
+
+        Optional<Role> optionalAdminRole = roleRepository.findByName(RoleEnum.ADMIN);
+        Optional<User> optionalAdminUser = userRepository.findByEmail(admin.getEmail());
+
+        if (optionalAdminRole.isEmpty() || optionalAdminUser.isPresent())  {
+            return;
+        }
+
+        var newAdmin = new User();
+        newAdmin.setName(admin.getName());
+        newAdmin.setLastname(admin.getLastname());
+        newAdmin.setEmail(admin.getEmail());
+        newAdmin.setPassword(passwordEncoder.encode(admin.getPassword()));
+        newAdmin.setRole(optionalAdminRole.get());
+
+        userRepository.save(newAdmin);
     }
 
     private void createUser() {
 
         User user = new User();
-        user.setName("Jose");
-        user.setLastname("Vargas");
-        user.setEmail("nachito8600@gmail.com");
-        user.setPassword("9Varguitas");
+        user.setName("User01");
+        user.setLastname("System");
+        user.setEmail("User01@gmail.com");
+        user.setPassword("User");
 
         Optional<Role> optionalRole = roleRepository.findByName(RoleEnum.USER);
         Optional<User> optionalUser = userRepository.findByEmail(user.getEmail());
